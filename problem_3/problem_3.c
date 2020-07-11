ba114ed458bec812632083cab32d5a65827e1527
Neo Cass <neo@applesand.home>
2020-07-11 21:59:10 +0100

    p3

--- problem_3/problem_3.c
+++ problem_3/problem_3.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
+#include <math.h>
+
+int largestPFactor(int num){
+    int largest;
+    for(int i = 1; i<=sqrt(num); i++){
+        if(num%i==0){
+            if(largestPFactor(i)==i){
+                largest=i;
+            }
+            
+        }
+    }
+    return largest;
+}
 
 int main(){
-    printf("%d", 1);
+    printf("%d", largestPFactor(10));
     return 1;
 }
