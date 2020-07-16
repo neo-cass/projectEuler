f6ea11e583c11859b956b8ee7e9331f766f73058
Neo Cass <neo@applesand.home>
2020-07-16 17:47:02 +0100

    p3

--- problem_3/problem_3.c
+++ problem_3/problem_3.c
@@ -1,20 +1,39 @@
 #include <stdio.h>
 #include <math.h>
 
-int largestPFactor(int num){
-    int largest;
-    for(int i = 1; i<=sqrt(num); i++){
+int isPrime(long num){
+    for(long i = 2; i<sqrt(num); i++){
         if(num%i==0){
-            if(largestPFactor(i)==i){
-                largest=i;
-            }
-            
+            return 0;
         }
     }
+    return 1;
+}
+
+
+long largestPFactor(long num){
+    long largest=0;
+    for(long i = 2; i<=sqrt(num); i++){
+        if(num%i==0){
+            long ipair = num/i;
+            int iBool = isPrime(i)==1 && i>largest;
+            int ipairBool = isPrime(ipair)==1 && ipair>largest;
+
+            if(iBool){
+                largest=i;
+            }
+            if(ipairBool){
+                largest = ipair;
+            }
+        }
+    }
+    if(largest==0){largest=num;}
     return largest;
 }
 
+
+
 int main(){
-    printf("%d", largestPFactor(10));
+    printf("%ld\n\n\n", largestPFactor((long)600851475143));
     return 1;
 }
